a449e0c58e35e152f8fb09b0b5695201d3b9b36c
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-11-16 17:30:27 +0100

    add date range picker component

--- components/global/BookHeroSection.tsx
+++ components/global/BookHeroSection.tsx
@@ -7,13 +7,14 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-import { Plus,Minus } from 'lucide-react'
+import { Plus, Minus } from "lucide-react";
+import { DatePickerWithRange } from "./DatePickerWithRange";
 type Props = {};
 
 const BookHeroSection = (props: Props) => {
   return (
     <div className="flex-1 text-foreground p-2 flex gap-2 h-[70px] max-w-2xl bg-[#fff3] ">
-      <Input className="h-full text-lg" type="date" />
+      <DatePickerWithRange />
       <Popover>
         <PopoverTrigger asChild>
           <Button variant={"outline"} className="h-full px-12 text-lg">
@@ -21,24 +22,32 @@ const BookHeroSection = (props: Props) => {
           </Button>
         </PopoverTrigger>
         <PopoverContent>
-            <div>
-                <div className="py-4 px-2 flex justify-between items-center">
-                <h4>Adults</h4>
-                <div className="flex gap-4 items-center">
-                    <Button size="icon" variant={"outline"}><Minus/></Button>
-                    <div>6</div>
-                    <Button size="icon" variant={"outline"}><Plus/></Button>
-                </div>
-                </div>
-                <div className="py-4 px-2 flex justify-between items-center">
-                <h4>children</h4>
-                <div className="flex gap-4 items-center">
-                    <Button size="icon" variant={"outline"}><Minus/></Button>
-                    <div>6</div>
-                    <Button size="icon" variant={"outline"}><Plus/></Button>
-                </div>
-                </div>
+          <div>
+            <div className="py-4 px-2 flex justify-between items-center">
+              <h4>Adults</h4>
+              <div className="flex gap-4 items-center">
+                <Button size="icon" variant={"outline"}>
+                  <Minus />
+                </Button>
+                <div>6</div>
+                <Button size="icon" variant={"outline"}>
+                  <Plus />
+                </Button>
+              </div>
             </div>
+            <div className="py-4 px-2 flex justify-between items-center">
+              <h4>children</h4>
+              <div className="flex gap-4 items-center">
+                <Button size="icon" variant={"outline"}>
+                  <Minus />
+                </Button>
+                <div>6</div>
+                <Button size="icon" variant={"outline"}>
+                  <Plus />
+                </Button>
+              </div>
+            </div>
+          </div>
         </PopoverContent>
       </Popover>
 
