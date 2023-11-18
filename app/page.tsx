518e36492df5838152f53c7828319faebe196893
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-11-18 15:18:13 +0100

    add home Page Hotel section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from '@/components/global/HeroSection';
 import RoomsSlider from '@/components/global/RoomsSlider';
 import HomePageTopSection from '@/components/global/HomePageTopSection';
+import HomePageHotelSection from '@/components/global/HomePageHotelSection';
 
 export default function Home() {
   return (
     <div className="min-h-[200vh] font-josefin">
       <HeroSection />
       <HomePageTopSection />
+      <HomePageHotelSection />
       <RoomsSlider />
     </div>
   );
